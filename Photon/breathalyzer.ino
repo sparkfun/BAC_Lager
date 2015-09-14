68358b82211ca39dadfdbfba9101f0cbd3d9958d
ShawnHymel <shawn.hymel@sparkfun.com>
2015-09-14 11:27:31 -0600

    Updated license in Photon .ino file

--- Photon/breathalyzer.ino
+++ Photon/breathalyzer.ino
@@ -8,15 +8,8 @@
  * 
  * Data channel: https://data.sparkfun.com/sfe_bac
  * 
- * License: http://opensource.org/licenses/MIT
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * There's really only one appropiate license for this code:
+ * https://en.wikipedia.org/wiki/Beerware
  */
 
 // This #include statement was automatically added by the Particle IDE.
